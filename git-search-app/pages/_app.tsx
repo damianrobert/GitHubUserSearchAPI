ee127c3948985f879ae0a084d30ad0cb268345d9
damianrobert <damianrobert666@yahoo.com>
2023-02-08 15:04:22 +0200

    style config

--- git-search-app/pages/_app.tsx
+++ git-search-app/pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Space_Mono } from "@next/font/google";
+import { ThemeProvider } from "next-themes";
+
+const monoSpace = Space_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <div className={`${monoSpace.className}`}>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- git-search-app/tailwind.config.js
+++ git-search-app/tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBlue: "var(--primary-blue)",
+        lightGray: "var(--light-gray)",
+        lightBluePale: "var(--light-blue-pale)",
+        tealishBlue: "var(--tealish-blue)",
+        ghostWhite: "var(--ghost-white)",
+        secondaryWhite: "var(--secondary-white)",
+        darkBlue: "var(--dark-blue)",
+        darkTealishBlue: "var(--dark-tealish-blue)",
+      },
+    },
   },
   plugins: [],
-}
+};
